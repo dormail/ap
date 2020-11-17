1930ce935c462152dc6d1fbd26ec48a6e3024f83
Matthias Maile <matthias.maile@udo.edu>
2020-11-17 19:20:23 +0100

    3 haessliche plots

--- V103/plot.py
+++ V103/plot.py
@@ -1,21 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+df1 = pd.read_csv('daten1.csv')
+df2 = pd.read_csv('daten2.csv')
+df3 = pd.read_csv('daten3.csv')
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+print(df1.head())
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+# messung 1
+# stab der an beiden enden aufliegt
+d1 = df1['stellung der messuhr in cm']
+x1 = df1['biegung in micrometer']
+
+plt.scatter(d1,x1, c='k', marker='+')
+plt.savefig('build/plot1.pdf')
+
+plt.close()
+
+# messung 2
+# stab1 mit einem freien ende
+d2 = df2['stellung der messuhr in cm']
+x2 = df2['biegung in micrometer']
+
+plt.scatter(d2,x2, c='k', marker='+')
+plt.savefig('build/plot2.pdf')
+plt.close()
+
+# messung 3
+# stab2 mit einem freien ende
+d3 = df3['stellung der messuhr in cm']
+x3 = df3['biegung in micrometer']
+
+plt.scatter(d3,x3, c='k', marker='+')
+plt.savefig('build/plot3.pdf')
+
+
+#plt.show()
 
 # in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
