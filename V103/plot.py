0af1cd119d32cf69f3af3615b2f053d0da365c6a
Matthias Maile <matthias.maile@udo.edu>
2020-11-21 16:30:40 +0100

    auswertung fuer messung 1

--- V103/plot.py
+++ V103/plot.py
@@ -24,7 +24,8 @@ print(f'Dicht von Stab 1:\t{dichte1} kg/m^3')
 print(f'Dicht von Stab 2:\t{dichte2} kg/m^3')
 
 
-### die funktion was die auslenkung definiert ###
+### die funktionen für theoriekurven ###
+# einseitig
 def D(x, E):
     r = 0.005 # radius der staebe in meter
     m = 0.949 # masse des gewichtes in Kg
@@ -34,6 +35,29 @@ def D(x, E):
     I = np.pi * r**4 / 4 # flaechentraegheistmoment
     L = 0.53 # hebelarm in metern
     return F / (2 * E * I) * (L * x**2 - x**3 / 3)
+
+# beidseitig
+def D_mit(x, E):
+    r = 0.005 # radius der staebe in meter
+    m = 0.949 # masse des gewichtes in Kg
+    
+    F = m * 9.81 # Kraft durch die Masse m
+    
+    I = np.pi * r**4 / 4 # flaechentraegheistmoment
+    L = 0.53 # hebelarm in metern
+    # return F / (48 * E * I) * (4 * x**3 - 12 * L * x**2 + 9 * L**2 * x - L**3)
+    return F / (48 * E * I) * (3 * L**2 * x - 4 * x**3)
+
+def D_mit_left(x,E):
+    r = 0.005 # radius der staebe in meter
+    m = 0.949 # masse des gewichtes in Kg
+    
+    F = m * 9.81 # Kraft durch die Masse m
+    
+    I = np.pi * r**4 / 4 # flaechentraegheistmoment
+    L = 0.53 # hebelarm in metern
+    return F / (48 * E * I) * (4 * x**3 - 12 * L * x**2 + 9 * L**2 * x - L**3)
+
 
 # allgemeine Daten
 r = 0.005 # radius der staebe in meter
@@ -45,21 +69,63 @@ I = np.pi * r**4 / 4 # flaechentraegheistmoment
 
 # messung 1
 # stab der an beiden enden aufliegt
-L = 28.5 # aufhaengepunkt in cm
+L = 0.285 # mitte in cm
 
 df1 = pd.read_csv('daten/daten1_1.csv')
 df2 = pd.read_csv('daten/daten1_2.csv')
-x1 = df1['stellung der messuhr in cm']
-x2 = df2['stellung der messuhr in cm']
+x1 = df1['stellung der messuhr in cm'] * 0.01
+x2 = df2['stellung der messuhr in cm'] * 0.01
 
-D1 = df1['biegung in micrometer']
-D2 = df2['biegung in micrometer']
+D1 = df1['biegung in micrometer'] / 1000000 # nun in metern
+D2 = df2['biegung in micrometer'] / 1000000 # nun in metern
+
+# fuer den fit muss eine seite auf die andere gebracht werden
+pos_right = np.append(x1[x1 < 0.285], x2[x2 < .285])
+pos_left = np.append(x1[x1 > 0.285], x2[x2 > .285])
+data_right = np.append(D1[x1 < 0.285], D2[x2 < .285])
+data_left = np.append(D1[x1 > 0.285], D2[x2 > .285])
+
+print(len(pos_right))
+print(len(pos_left))
+print(len(data_right))
+print(len(data_left))
+
+fitting_data = np.append(data_left, data_right)
+pos_mirrored = (.285 - (pos_left - .285))
+#print(pos_right_to_left)
+fitting_x = np.append(pos_mirrored, pos_right)
+#fitting_x = pos_left
+#fitting_data = data_left
+
+print(fitting_x)
+print(fitting_data)
+
+# plt.close()
+# plt.scatter(fitting_x, fitting_data, c='r')
+#plt.scatter(pos_mirrored, fitting_data, c='b')
+popt, pcov = opt.curve_fit(D_mit, fitting_x, fitting_data)
+E = popt[0]
+print(f'E-Modul fuer Stab 1 nach Messung 1:\t{E:.4}')
+
+# plot der Theoriekurve
+x = np.linspace(0,0.285)
+plt.plot(x, D_mit(x,E), 'r',
+        label=rf'Theoriekurve für $E={{{E:.4}}}$')
+x = np.linspace(.285,0.57)
+plt.plot(x, D_mit_left(x,E), 'r')
 
 lx1 = np.abs(x1 - L)
 lx2 = np.abs(x2 - L)
 
-plt.scatter(lx1, D1, c='b', marker='+')
-plt.scatter(lx2, D2, c='g', marker='+')
+plt.scatter(x1, D1, c='b', marker='+', label='Messuhr 1')
+plt.scatter(x2, D2, c='g', marker='+', label='Messuhr 2')
+plt.scatter(0.64,130 / 1000000, c='r', marker='+', label='Fehlerhafte Messung')
+
+plt.xlabel(r'$x / \si{m}$')
+plt.ylabel(r'$D(x) / \si{m}$')
+plt.title(rf'Messdaten und Fit zu Stab 1, beidseitig eingespannt')
+plt.legend()
+
 plt.savefig('build/plot1.pdf')
 
 plt.close()
@@ -82,14 +148,15 @@ plt.scatter(x1, D1, c='b', marker='+',
     label='Messuhr 1')
 plt.scatter(x2, D2, c='g', marker='+',
         label='Messuhr 2')
-plt.title('Stab 1, einseitig eingespannt')
+#plt.title('Stab 1, einseitig eingespannt')
+plt.title(rf'Messdaten und Fit zu Stab 1, einseitig eingespannt')
 
 # curve fit
 x_all = np.append(x1, x2)
 D_all = np.append(D1, D2)
 popt, pcov = opt.curve_fit(D, x_all, D_all)
 E = popt[0]
-print(f'Elastizitätsmodul für Stab 1: \t {E}')
+print(f'E-Modul für Stab 1 nach Messung 2: \t {E:.4}')
 
 # plot der Theoriekurve
 x = np.linspace(0,L)
@@ -97,22 +164,27 @@ plt.plot(x, D(x,E), 'r',
         label=rf'Theoriekurve für $E={{{E:.4}}}$')
         #label=rf'Theoriekurve für $E={{{E:.4}}} \, \frac{{N}}{{m^2}}$')
 
-plt.xlabel(r'$\frac{x}{cm}$')
+c = np.sqrt(E / dichte1)
+print(f'Schallgeschwindigkeit in Stab 1:\t{c}')
+
+#plt.xlabel(r'$\frac{x}{cm}$')
 plt.legend()
+plt.xlabel(r'$x / \si{m}$')
+plt.ylabel(r'$D(x) / \si{m}$')
 
 plt.savefig('build/plot2.pdf')
 plt.close()
 
 # messung 3
 # stab2 mit einem freien ende
-L = 53 # aufhaengepunkt in cm
+L = 0.53 # aufhaengepunkt in cm
 df1 = pd.read_csv('daten/daten3_1.csv')
 df2 = pd.read_csv('daten/daten3_2.csv')
-x1 = df1['stellung der messuhr in cm']
-x2 = df2['stellung der messuhr in cm']
+x1 = df1['stellung der messuhr in cm'] * 0.01
+x2 = df2['stellung der messuhr in cm'] * 0.01
 
-D1 = df1['biegung in micrometer']
-D2 = df2['biegung in micrometer']
+D1 = df1['biegung in micrometer'] / 1000000
+D2 = df2['biegung in micrometer'] / 1000000
 
 lx1 = np.abs(L - x1)
 lx2 = np.abs(L - x2)
@@ -121,9 +193,29 @@ plt.scatter(x1, D1, c='b', marker='+',
     label='Messuhr 1')
 plt.scatter(x2, D2, c='g', marker='+',
         label='Messuhr 2')
-plt.title('Stab 2, einseitig eingespannt')
+
+# curve fit
+x_all = np.append(x1, x2)
+D_all = np.append(D1, D2)
+popt, pcov = opt.curve_fit(D, x_all, D_all)
+E = popt[0]
+print(f'Elastizitätsmodul für Stab 2: \t {E}')
+
+# plot der Theoriekurve
+x = np.linspace(0,L)
+plt.plot(x, D(x,E), 'r', 
+        label=rf'Theoriekurve für $E={{{E:.4}}}$')
+        #label=rf'Theoriekurve für $E={{{E:.4}}} \, \frac{{N}}{{m^2}}$')
+
+c = np.sqrt(E / dichte2)
+print(f'Schallgeschwindigkeit in Stab 2:\t{c}')
+
 plt.xlabel(r'$\frac{x}{cm}$')
 plt.legend()
+#plt.title('Stab 2, einseitig eingespannt')
+plt.title(rf'Messdaten und Fit zu Stab 2, einseitig eingespannt')
+plt.xlabel(r'$x / \si{m}$')
+plt.ylabel(r'$D(x) / \si{m}$')
 
 plt.savefig('build/plot3.pdf')
 plt.close()
