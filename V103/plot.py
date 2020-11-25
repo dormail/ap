65215c327399d587be778d2a2e77823173150504
Matthias Maile <matthias.maile@udo.edu>
2020-11-25 19:59:35 +0100

    code cleanup messung 1

--- V103/plot.py
+++ V103/plot.py
@@ -67,63 +67,48 @@ F = m * 9.81 # Kraft durch die Masse m
 
 I = np.pi * r**4 / 4 # flaechentraegheistmoment
 
-# messung 1
+### messung 1 ###
 # stab der an beiden enden aufliegt
 L = 0.285 # mitte in cm
 
+### data import ###
 df1 = pd.read_csv('daten/daten1_1.csv')
 df2 = pd.read_csv('daten/daten1_2.csv')
 x1 = df1['stellung der messuhr in cm'] * 0.01
 x2 = df2['stellung der messuhr in cm'] * 0.01
-
-x1_messung1 = x1
-x2_messung1 = x2
-
 D1 = df1['biegung in micrometer'] / 1000000 # nun in metern
 D2 = df2['biegung in micrometer'] / 1000000 # nun in metern
 
-D1_messung1 = D1
-D2_messung1 = D2
-
+### data cleaning ###
 # fuer den fit muss eine seite auf die andere gebracht werden
 pos_right = np.append(x1[x1 < 0.285], x2[x2 < .285])
 pos_left = np.append(x1[x1 > 0.285], x2[x2 > .285])
 data_right = np.append(D1[x1 < 0.285], D2[x2 < .285])
 data_left = np.append(D1[x1 > 0.285], D2[x2 > .285])
 
-print(len(pos_right))
-print(len(pos_left))
-print(len(data_right))
-print(len(data_left))
-
 fitting_data = np.append(data_left, data_right)
 pos_mirrored = (.285 - (pos_left - .285))
-#print(pos_right_to_left)
 fitting_x = np.append(pos_mirrored, pos_right)
-#fitting_x = pos_left
-#fitting_data = data_left
 
 print(fitting_x)
 print(fitting_data)
 
-# plt.close()
-# plt.scatter(fitting_x, fitting_data, c='r')
-#plt.scatter(pos_mirrored, fitting_data, c='b')
+# curve-fit 
 popt, pcov = opt.curve_fit(D_mit, fitting_x, fitting_data)
+error = np.sqrt(np.diag(pcov))
 E = popt[0]
+e = error[0]
 
 print(f'E-Modul fuer Stab 1 nach Messung 1:\t{E:.4}')
-error = np.sqrt(np.diag(pcov))
-e = error[0]
 print(f'Abweichung: \t{error[0]:.4}')
 
 
 
-# plot der Theoriekurve
+### plot der Theoriekurve ###
 # linke haelfte
 x = np.linspace(0,0.285)
 plt.plot(x, D_mit(x,E), 'r',
-        label=rf'Theoriekurve für $E=({{{E:.4}}})$Pa')
+        label=rf'Theoriekurve für $E=({{{E:.5}}})$Pa')
 plt.fill_between(x, D_mit(x,E+e), D_mit(x,E-e), 
         facecolor='red', alpha=0.5, label=rf'$\sigma$-Umgebung')
 # rechte haelfte
@@ -132,9 +117,7 @@ plt.plot(x, D_mit_left(x,E), 'r')
 plt.fill_between(x, D_mit_left(x,E+e), D_mit_left(x,E-e), 
         facecolor='red', alpha=0.5)
 
-# lx1 = np.abs(x1 - L)
-# lx2 = np.abs(x2 - L)
-
+# datenpunkte
 plt.scatter(x1, D1, c='b', marker='+', label='Messuhr 1')
 plt.scatter(x2, D2, c='g', marker='+', label='Messuhr 2')
 plt.scatter(0.64,130 / 1000000, c='r', marker='+', label='Fehlerhafte Messung')
@@ -147,6 +130,12 @@ plt.legend()
 plt.savefig('build/plot1.pdf')
 
 plt.close()
+# sicherung in extra vektoren falls die am ende nochmal gebraucht werden
+D1_messung1 = D1
+D2_messung1 = D2
+x1_messung1 = x1
+x2_messung1 = x2
+
 
 # messung 2
 # stab1 mit einem freien ende
