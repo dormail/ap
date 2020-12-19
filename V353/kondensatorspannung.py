3a2f6f45e03401bddc849056a72cb69546be6f1a
Matthias Maile <matthias.maile@udo.edu>
2020-12-19 11:01:35 +0100

    plot und abweichung added

--- V353/kondensatorspannung.py
+++ V353/kondensatorspannung.py
@@ -1,7 +1,9 @@
 ### kondensatorspannung.py ###
+# fuer Messung 4b
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
 # daten input, anpassung auf SI-Größen
 df = pd.read_csv('daten/kondensatorspannung1.csv')
@@ -18,8 +20,34 @@ U0 = 4.2
 f = np.append(f1, f2)
 A = np.append(A1, A2)
 
+# curve fit
+def AC(f, T):
+    U0 = 4.2
+    w = 2 * np.pi * f
+    return U0 / (np.sqrt(1 + w**2 * T**2))
+
+p0 = [0.0008]
+om = 2 * np.pi * f
+popt, pcov = curve_fit(AC, f, A, p0=p0)
+#U0 = popt[0]
+U0 = 4.2
+T = popt[0]
+err = np.sqrt(np.diag(pcov))[0]
+
+# output
+#print(f'U0 = {U0:.4}')
+print(f'RC = {T:.4} ± {err:.3}')
+
 # plot
-plt.scatter(f,A)
+f_plt = np.linspace(3*10**2,10**5,10**6)
+plt.scatter(f,A, marker='+', label='Messdaten')
+plt.plot(f_plt, AC(f_plt, T), color='r', label=rf'Curve Fit zu $RC={{{T * 10**6:.5}}}\mu\si{{s}}$',
+        linewidth=0.4)
 
+plt.title(r'Kondensatorspannung $U_C(\omega)$ bei $U_0 = 4.2\si{V}$')
+plt.ylabel(r'$U_C / \si{V}$')
+plt.xlabel(r'$\omega \cdot \si{s}$')
+plt.xscale('log')
+plt.legend()
 
-plt.savefig('spannungsverhaeltnis.pdf')
+plt.savefig('build/4b.pdf')
