d7103019182edfb47be6fed39bf104c79cfba2a3
Matthias Maile <matthias.maile@udo.edu>
2021-01-26 08:28:38 +0100

    untergrundrate done

--- D702/untergrundrate.py
+++ D702/untergrundrate.py
@@ -7,15 +7,18 @@ from uncertainties import unumpy, ufloat
 
 def get_untergrund():
     # daten input
+    t = 300
     df = pd.read_csv('daten/untergrundrate.csv')
     N = df['N_U']
     
     # daten sind poisson verteilt, daher abweichung DN = sqrt(N)
     DN = np.sqrt(N)
     N = unumpy.uarray(N, DN)
+    #print(N)
     
     #
     N_U = sum(N) / len(N)
+    #N_U = N_U / t # falls man es auf 1 sekunde normieren moechte
     return N_U
 
 N_U = get_untergrund()
