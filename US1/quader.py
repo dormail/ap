d413ba751d2bd9a741ffde8c46d148a2f9736650
Philip Jaletzky <philip.jaletzky@tu-dortmund.de>
2021-04-19 20:55:41 +0200

    auswertung fertig+diskussion

--- US1/quader.py
+++ US1/quader.py
@@ -8,12 +8,12 @@ import uncertainties.unumpy as unp
 from uncertainties.unumpy import (nominal_values as noms,
                                   std_devs as stds)
 
+
 s, t = np.genfromtxt('content/quaderdaten.txt', unpack=True)
-print(s)
-print(t)
+
 S=np.sort(s)
 T=np.sort(t)
-print(S)
+
 params, covariance_matrix = np.polyfit(T, S, deg =1 , cov = True)
 errors = np.sqrt(np.diag(covariance_matrix))
 print(f'a = {params[0]} +- {errors[0]}')
@@ -28,3 +28,8 @@ plt.savefig('build/plot.pdf')
 
 #daempfung
 a1, a2, d = np.genfromtxt('content/daempfung.txt', unpack=True)
+a=np.log(a2/a1)/d
+sd=np.std(a, ddof=1)
+alpha=np.mean(a)
+
+print(f'alpha = {alpha} +- {sd}')
