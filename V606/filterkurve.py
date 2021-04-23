3977f468621e213a4998c3883111361e4d8a4cd1
Matthias Maile <matthias.maile@udo.edu>
2021-04-23 22:26:23 +0200

    kack ergebnis aber egal

--- V606/filterkurve.py
+++ V606/filterkurve.py
@@ -4,35 +4,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from uncertainties.umath import *
 
 f, U = np.genfromtxt('Daten/filterkurve.txt', unpack=True)
 
 data = {'f': f, 'U': U}
 df = pd.DataFrame(data)
 df = df.sort_values('f')
-df.to_csv('build/filterkurve.csv')
+df.to_csv('build/filterkurve.tex', 
+        sep='&', 
+        header=False,
+        index=False,
+        line_terminator='\t\\\\\n')
 
 
 # fitten eine Gaussglocke drüber
-def gaus(x, m, s, n):
-    return n * np.exp(-1 * (x - m)**2 / (2 * s**2))
+def gaus(x, m, a, n):
+    return n * np.exp(-1 * a * (x - m)**2)
 
 popt, pcov = curve_fit(gaus, f, U, p0=[35, 5, 2])
-m = popt[0]
-s = popt[1]
-n = popt[2]
+errors = np.sqrt(np.diag(pcov))
+m = ufloat(popt[0], errors[0])
+a = ufloat(popt[1], errors[1])
+n = ufloat(popt[2], errors[2])
+print(f'm = {m} \na = {a} \nn = {n}')
+
+Q = m / sqrt(2 * np.log(2) / a)
+m = m.n
+a = a.n
+n = n.n
 
 print(f'Fitparameter:')
-print(f'm = {m} \ns = {s} \nn = {n}')
-print(f'gaus(m)/ sqrt(2) = {gaus(m, m, s, n) / np.sqrt(2)}')
-print(f'gaus(m-5) = {gaus(m-5, m, s, n)}')
+print(f'gaus(m)/ sqrt(2) = {gaus(m, m, a, n) / np.sqrt(2)}')
+print(f'gaus(m-5) = {gaus(m-5, m, a, n)}')
 
-print(f'Güte Q = {35 / 10}')
+print(f'Güte Q = {Q}')
+print(f'{sqrt(2 * np.log(2) / a)}')
 
 x = np.linspace(20, 50, 200)
 plt.scatter(f,U, color='k', marker='+',
         label='Messdaten')
-plt.plot(x, gaus(x,m,s,n), color='r',
+plt.plot(x, gaus(x,m,a,n), color='r',
         label='Curve Fit')
 
 plt.xlabel(r'$f$ / kHz')
